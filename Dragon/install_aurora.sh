a857941f713a85d8708b399c425fae7870e310d3
Christine Simpson <csimpson@anl.gov>
2025-12-09 18:05:51 +0000

    updated aurora install script

--- Dragon/install_aurora.sh
+++ Dragon/install_aurora.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-WDIR=$PWD
-DRAGON_DIR=$1
-
 # Load modules
-module load frameworks
+module load tensorflow
 
 # Create venv
-cd $DRAGON_DIR
-python -m venv --clear _dragon_env --system-site-packages
+python -m venv _dragon_env --system-site-packages
 . _dragon_env/bin/activate
-pip install --force-reinstall dragon*.whl
+pip install dragonhpc
+dragon-config add --ofi-runtime-lib=/opt/cray/libfabric/1.22.0/lib64
 
 # Install other packages
 pip install -i https://pypi.anaconda.org/OpenEye/simple OpenEye-toolkits
 pip install SmilesPE
 pip install MDAnalysis rdkit
 pip install pydantic_settings
+pip install pandas
+pip install transformers
+pip install mpi4py
+pip install scikit-learn scikit-learn-intelex
